7f954029913101fd88f5cac8507aae567c212aee
jdubya80 <jasonwstubbs@gmail.com>
2016-02-25 02:08:41 -0700

    Add plugins sh... need to make it so it auto creates the folder for the bundles

---
+++ vi_plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ack.vim
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
+git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
